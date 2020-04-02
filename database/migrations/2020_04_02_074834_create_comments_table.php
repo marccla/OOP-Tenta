a81e662a7174fc75e86d3ecada80a23e1ecb5409
marccla <marcclarsson@gmail.com>
2020-04-02 10:50:12 +0200

    working on databases

---
+++ database/migrations/2020_04_02_074834_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            // $table->index('post_id');
+            $table->foreignId('post_id')->references('id')->on('posts');
+            $table->longText('content');
+            $table->string('author');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
